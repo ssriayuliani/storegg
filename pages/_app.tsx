a970f673d01a18b44a37382a61f67fce484206aa
Lia <sriayuliani1514@gmail.com>
2021-09-17 00:01:24 +0800

    Convert Sign-Up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-/* eslint-disable linebreak-style */
 import '../styles/utilities.css';
 import '../styles/homepage.css';
 import '../styles/detail.css';
 import '../styles/checkout.css';
 import '../styles/complete-checkout.css';
+import '../styles/sign-in.css';
+import '../styles/sign-up.css';
+import '../styles/sign-up-photo.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
