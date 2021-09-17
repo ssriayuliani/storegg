778f6ff98716c3718960245acb67fb54728ed14a
Lia <sriayuliani1514@gmail.com>
2021-09-17 23:21:08 +0800

    Refactor ddetail, component TopUp

--- components/organisms/TopUpItem/index.tsx
+++ components/organisms/TopUpItem/index.tsx
@@ -1,4 +1,22 @@
-export default function TopUpItem() {
+interface TopUpItemProps{
+  type: 'desktop' | 'mobile';
+}
+
+export default function TopUpItem(props : TopUpItemProps) {
+  const { type } = props;
+
+  if (type === 'desktop') {
+    return (
+      <div className="pb-50 d-md-block d-none">
+        <h2 className="text-4xl fw-bold color-palette-1 text-start mb-10 mt-10">
+          Mobile Legends:
+          <br />
+          The New Battle 2021
+        </h2>
+        <p className="text-lg color-palette-2 mb-0">Category: Mobile</p>
+      </div>
+    );
+  }
   return (
     <div className="row align-items-center">
       <div className="col-md-12 col-4">
--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -11,17 +11,10 @@ export default function Detail() {
         </div>
         <div className="row">
           <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
-            <TopUpItem />
+            <TopUpItem type="mobile" />
           </div>
           <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
-            <div className="pb-50 d-md-block d-none">
-              <h2 className="text-4xl fw-bold color-palette-1 text-start mb-10 mt-10">
-                Mobile Legends:
-                <br />
-                The New Battle 2021
-              </h2>
-              <p className="text-lg color-palette-2 mb-0">Category: Mobile</p>
-            </div>
+            <TopUpItem type="desktop" />
             <hr />
             <TopUpForm />
           </div>
