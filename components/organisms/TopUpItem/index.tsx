3611da65f036d0c416e73d049cf34f29c688898e
Lia <sriayuliani1514@gmail.com>
2021-09-17 21:57:33 +0800

    delete detail/id.tsx and Refactor detail page

---
+++ components/organisms/TopUpItem/index.tsx
@@ -0,0 +1,17 @@
+export default function TopUpItem() {
+  return (
+    <div className="row align-items-center">
+      <div className="col-md-12 col-4">
+        <img src="/img/Thumbnail-3.png" width="280" height="380" className="img-fluid" alt="" />
+      </div>
+      <div className="col-md-12 col-8 d-md-none d-block">
+        <h2 className="text-xl fw-bold color-palette-1 text-start mb-10">
+          Mobile Legends:
+          <br />
+          The New Battle 2021
+        </h2>
+        <p className="text-sm color-palette-2 text-start mb-0">Category: Mobile</p>
+      </div>
+    </div>
+  );
+}
