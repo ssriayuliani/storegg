bcaeba7edab3b27306fe48264a32f5b8de418231
Lia <sriayuliani1514@gmail.com>
2021-09-18 00:09:47 +0800

    Refactor Checkout

--- components/organisms/TopUpItem/index.tsx
+++ components/organisms/TopUpItem/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface TopUpItemProps{
   type: 'desktop' | 'mobile';
 }
@@ -20,7 +22,7 @@ export default function TopUpItem(props : TopUpItemProps) {
   return (
     <div className="row align-items-center">
       <div className="col-md-12 col-4">
-        <img src="/img/Thumbnail-3.png" width="280" height="380" className="img-fluid" alt="" />
+        <Image src="/img/Thumbnail-3.png" width={280} height={380} className="img-fluid" alt="" />
       </div>
       <div className="col-md-12 col-8 d-md-none d-block">
         <h2 className="text-xl fw-bold color-palette-1 text-start mb-10">
