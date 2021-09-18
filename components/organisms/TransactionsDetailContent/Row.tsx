aefb30d13f4586bb85f39f0944baa11221a99a6e
Lia <sriayuliani1514@gmail.com>
2021-09-18 19:49:36 +0800

    Refactor TransactionDetai

---
+++ components/organisms/TransactionsDetailContent/Row.tsx
@@ -0,0 +1,20 @@
+interface RowProps{
+    title:string;
+    value: string|number;
+    className ?: string;
+}
+
+export default function Row(props: Partial<RowProps>) {
+  const { title, value, className } = props;
+  return (
+    <p className="text-lg color-palette-1 mb-20">
+      {title}
+      {' '}
+      <span
+        className={`purchase-details ${className}`}
+      >
+        {value}
+      </span>
+    </p>
+  );
+}
